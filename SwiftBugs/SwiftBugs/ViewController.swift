8c722bbb414e8e9531940b2f64fdbb281c55e05b
Heath Borders <heath.borders@gmail.com>
2019-05-02 20:51:00 -0500

    Empty project

---
+++ SwiftBugs/SwiftBugs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBugs
+//
+//  Created by Heath Borders on 5/2/19.
+//  Copyright © 2019 Heath Borders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
